6a7fdcc5f9f81690fc6156184bba15db67ed3c0b
Umidjon <makhammadsoliyev@gmail.com>
2025-10-15 04:00:51 +0500

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var first = Convert.ToDouble(txbFirstNumber.Text);
+            var second = Convert.ToDouble(txbSecondNumber.Text);
+
+            lblSumResult.Content = first + " + " + second + " = " + (first + second);
+            lblMinResult.Content = first + " - " + second + " = " + (first - second);
+            lblMulResult.Content = first + " * " + second + " = " + (first * second);
+            lblDivResult.Content = first + " / " + second + " = " + (first / second);
+        }
+    }
+}
